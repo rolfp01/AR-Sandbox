d4d87833aedc9a1c134bdc00b9dbb4ec3f6f3e7a
rolfp01 <piamelina@t-online.de>
2025-09-01 17:29:48 +0200

    line length

--- DataRead/readAsusXtionCamera.py
+++ DataRead/readAsusXtionCamera.py
@@ -4,14 +4,14 @@ def read_Depth_Camera(color_stream, depth_stream):
     # Lese ein Frame vom Farbsensor
     color_frame = color_stream.read_frame()
     color_data = color_frame.get_buffer_as_uint8()
-    color_image_unshaped = np.frombuffer(color_data, dtype=np.uint8)
-    color_image = color_image_unshaped.reshape((color_frame.height, color_frame.width, 3))
+    c_i_un = np.frombuffer(color_data, dtype=np.uint8)
+    color_image = c_i_un.reshape((color_frame.height, color_frame.width, 3))
 
     # Lese ein Frame vom Tiefensensor
     depth_frame = depth_stream.read_frame()
     depth_data = depth_frame.get_buffer_as_uint16()
-    depth_image_unshaped = np.frombuffer(depth_data, dtype=np.uint16)
-    depth_image = depth_image_unshaped.reshape((depth_frame.height, depth_frame.width))
+    d_i_un = np.frombuffer(depth_data, dtype=np.uint16)
+    depth_image = d_i_un.reshape((depth_frame.height, depth_frame.width))
 
     return {"color" :color_image, "depth":depth_image}
 
