4d8b646bc14bab256c4e83d34a23e9ba3be02ddb
rolfp01 <piamelina@t-online.de>
2025-09-30 17:13:07 +0200

    guter zwischenstand straße

--- DataCalculation/detectBuildings.py
+++ DataCalculation/detectBuildings.py
@@ -74,39 +74,75 @@ def detect_Buildings(depth_image, color_image, depth_scale, baseline_distance):
 
 
     # *** Straßen-Erkennung auf Basis von Farbe/Form ***
-    # 1. Farbbasierte Grau-Erkennung im HSV
     hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
-    v_channel = hsv[:, :, 2]
+    h_channel, s_channel, v_channel = cv2.split(hsv)
+    
+    # Dunkle, wenig gesättigte Bereiche (graue Straßen)
+    dark_mask = cv2.inRange(v_channel, 0, 130)  # Etwas höher
+    low_sat_mask = cv2.inRange(s_channel, 0, 100)  # Von 0 an
+    gray_roads = cv2.bitwise_and(dark_mask, low_sat_mask)
+    
+    # Blaue Straßen (dunkelblaue Objekte) - ERWEITERT
+    blue_hue_mask = cv2.inRange(h_channel, 85, 135)  # Breiterer Bereich
+    blue_sat_mask = cv2.inRange(s_channel, 30, 255)  # Niedrigere Untergrenze
+    blue_value_mask = cv2.inRange(v_channel, 40, 160)  # Spezifischer Value-Bereich für dunkelblaue Objekte
+    blue_roads = cv2.bitwise_and(blue_hue_mask, blue_sat_mask)
+    blue_roads = cv2.bitwise_and(blue_roads, blue_value_mask)
+    
+    # Kombiniere graue und blaue Straßen
+    road_candidate_mask = cv2.bitwise_or(gray_roads, blue_roads)
+    
+    # WICHTIG: Erst CLOSE (Lücken schließen), dann OPEN (Rauschen entfernen)
+    kernel_close = np.ones((9, 9), np.uint8)  # Größerer Kernel!
+    kernel_open = np.ones((3, 3), np.uint8)
+    
+    road_mask_clean = cv2.morphologyEx(road_candidate_mask, cv2.MORPH_CLOSE, kernel_close, iterations=2)
+    road_mask_clean = cv2.morphologyEx(road_mask_clean, cv2.MORPH_OPEN, kernel_open, iterations=1)
 
-    # Maske für dunkle Flächen
-    dark_mask = cv2.inRange(v_channel, 0, 120)
-
-    # Sättigung
-    s_channel = hsv[:, :, 1]
-    mask_saturation = cv2.inRange(s_channel, 10, 100)
-
-    # Kombiniere Masken: dunkel + moderate Sättigung (kein Schatten)
-    road_candidate_mask = cv2.bitwise_and(dark_mask, mask_saturation)
-
-    # Morphologische Filterung
-    kernel = np.ones((3, 3), np.uint8)
-    road_mask_clean = cv2.morphologyEx(road_candidate_mask, cv2.MORPH_OPEN, kernel)
-    road_mask_clean = cv2.morphologyEx(road_mask_clean, cv2.MORPH_CLOSE, kernel)
-
-    # Konturenerkennung wie gehabt
-    road_mask = np.zeros_like(road_mask_clean)
+    # Konturenerkennung mit GELOCKERTEN Kriterien
     contours, _ = cv2.findContours(road_mask_clean, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area < 200:
+        if area < 150:  # Niedrigere Schwelle
             continue
 
         x, y, w, h = cv2.boundingRect(cnt)
-        aspect_ratio = max(w / h, h / w)
+        aspect_ratio = max(w / h, h / w) if min(w, h) > 0 else 0
 
-        if aspect_ratio > 1.0:
+        # Sehr gelockerte Kriterien
+        if aspect_ratio > 1.2:  # Längliche Objekte
             cv2.drawContours(road_mask, [cnt], -1, 255, -1)
+        elif area > 500:  # Große Flächen auch ohne Längung
+            cv2.drawContours(road_mask, [cnt], -1, 255, -1)
+    
+    # ZUSÄTZLICH: Nochmal CLOSE auf dem Endergebnis
+    road_mask = cv2.morphologyEx(road_mask, cv2.MORPH_CLOSE, np.ones((11, 11), np.uint8), iterations=1)
+
+    # ===== SCHATTEN-PROBLEM BEHEBEN =====
+    # Schatten erscheinen als dunkle Bereiche neben Gebäuden
+    # Lösung: Entferne Straßen-Pixel, die direkt neben Gebäuden liegen
+    
+    # Erweitere Gebäude-Maske
+    kernel_dilate = np.ones((8, 8), np.uint8)  # Etwas kleiner
+    building_dilated = cv2.dilate(building_mask, kernel_dilate, iterations=1)
+    
+    # Schatten-Zone = erweiterte Gebäude MINUS Gebäude selbst
+    shadow_zone = cv2.bitwise_and(building_dilated, cv2.bitwise_not(building_mask))
+    
+    # Finde Straßen in der Schatten-Zone
+    roads_in_shadow = cv2.bitwise_and(road_mask, shadow_zone)
+    
+    # Entferne NUR sehr kleine Straßen-Fragmente in Shadow-Zone (= wahrscheinlich Schatten)
+    contours_shadow, _ = cv2.findContours(roads_in_shadow, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    shadow_to_remove = np.zeros_like(road_mask)
+    
+    for cnt in contours_shadow:
+        if cv2.contourArea(cnt) < 300:  # Nur kleine Fragmente = Schatten
+            cv2.drawContours(shadow_to_remove, [cnt], -1, 255, -1)
+    
+    # Entferne Schatten aus Straßen-Maske
+    road_mask = cv2.bitwise_and(road_mask, cv2.bitwise_not(shadow_to_remove))
 
     # *** Park-Erkennung auf Basis von Farbe ***
     # Annahme: Parks sind durch grüne Flächen repräsentiert (z.B. Grünfläche).
